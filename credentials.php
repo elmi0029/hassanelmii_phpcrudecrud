d62c2ed01db016523b0e8f10ac41df319b5d7d5b
Hassan Elmi <elmi0029@umn.edu>
2025-04-02 01:00:28 +0000

    Updated default search name in findemployee.html

--- credentials.php
+++ credentials.php
@@ -1,6 +1,6 @@
 <?php
 $servername = 'localhost';   // If Apache is on a different server from DB or using Docker, set IP address
 $dbname = 'employees';       // The name of the database you're going to use
-$username = 'hassanelmii';   // Your MySQL username
-$password = 'abc123';        // Your MySQL password
+$username = 'elmi0029';   // Your MySQL username
+$password = 'inet4031';        // Your MySQL password
 ?>>
--- testdbphp.php
+++ testdbphp.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Northland Analytics PHP/MySQL Test Page</title>
+    <title>Northland Analytics Employee Management System/MySQL Test Page</title>
 </head>
 <body>
 
-<h1>Northland Analytics PHP/MySQL Test Page</h1>
+<h1>Northland Analytics Employee Management System/MySQL Test Page</h1>
 
 <?php
-$conn = new mysqli('localhost', 'hassanelmii', 'abc123', 'employees');
+$conn = new mysqli('localhost', 'elmi0029', 'inet4031', 'employees');
 if ($conn->connect_error) {
     die("MySQL Connection Failed: " . $conn->connect_error);
 }
 
 echo "<p>Database Records Found</p>";
 
-$sql = "SELECT first_name, last_name, hire_date FROM employees WHERE last_name = 'Weedman'";
+$sql = "SELECT first_name, last_name, hire_date FROM employees WHERE last_name = 'John'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
